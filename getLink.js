58a8be086d37a49b4a3ab606d8c63b6ca3378ebc
iapheus <96398386+iapheus@users.noreply.github.com>
2022-02-17 07:06:51 +0300

    added getLink.js

---
+++ getLink.js
@@ -0,0 +1,9 @@
+var note = document.querySelectorAll(".c7hPc");
+var linkList = [];
+
+for (let index = 0; index <note.length; index++) {
+    linkList[index] = note[index].href
+}
+
+console.log(linkList)
+
